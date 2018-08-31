cd33a386adf305c17eef64328911bc0fe4f53af6
wecassidy <wec.notif.github@gmail.com>
2018-08-31 13:41:20 -0400

    Initial structure of template, Flask app

---
+++ rhythmlid.py
@@ -0,0 +1,21 @@
+"""
+Server-side of a web interface to the Rhythmbox music player.
+
+The server parses the Rhythmbox database to determine library
+information, then delivers albums, etc. to the web client to display.
+Playback is handled by an instance of Rhythmbox on the server
+computer.
+"""
+
+from flask import Flask, render_template, url_for, redirect
+from rhythmbox_library import MusicLibrary
+
+# Start the app
+app = Flask(__name__)
+
+# Load the library
+ml = MusicLibrary("~/.local/share/rhythmbox/rhythmdb.xml")
+
+@app.route("/<artist>")
+def artist_selected(artist):
+    return render_template("player.j2", library=ml, artist=artist, stylesheet=url_for("static", "style.css"))
